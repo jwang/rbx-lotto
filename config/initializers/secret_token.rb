5aead5770ac40f9f64bba3dc1d09df48b63bb875
John Wang <john@johntwang.com>
2011-07-21 21:43:13 -1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rbxlotto::Application.config.secret_token = '0d65fb3200d8f77ccd4c56ecbe3d92f987e95f6b7bb3c3de1eb8d71ae07a0462d77123429f6a124b86817a4a6cb6f2ca44756f1b8dca08c26971cfaee0d0778d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rbxlotto::Application.config.session_store :cookie_store, :key => '_rbxlotto_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rbxlotto::Application.config.session_store :active_record_store
