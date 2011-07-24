9a1180642ace7d126b81d94ff15993fb1ed738b6
John Wang <john@johntwang.com>
2011-07-24 11:25:01 -1000

    require name and amount and scope out remaining prizes

--- app/models/prize.rb
+++ app/models/prize.rb
@@ -1,4 +1,7 @@
 class Prize < ActiveRecord::Base
+  validates_presence_of :name, :on => :create, :message => "can't be blank"
+  validates_presence_of :amount, :on => :create, :message => "can't be blank"
   
+  scope :remaining, where("amount > 0")
   
 end
